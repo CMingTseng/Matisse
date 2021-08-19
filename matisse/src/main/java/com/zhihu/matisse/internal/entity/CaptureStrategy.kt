ca0b6b4aac8346d15714d537b9fe9f138b1849fb
Neo <c.min.tseng@gmail.com>
2021-08-19 12:54:37 +0800

    Convert CaptureStrategy to kotlin data class

--- matisse/src/main/java/com/zhihu/matisse/internal/entity/CaptureStrategy.kt
+++ matisse/src/main/java/com/zhihu/matisse/internal/entity/CaptureStrategy.kt
@@ -15,19 +15,4 @@
  */
 package com.zhihu.matisse.internal.entity;
 
-public class CaptureStrategy {
-
-    public final boolean isPublic;
-    public final String authority;
-    public final String directory;
-
-    public CaptureStrategy(boolean isPublic, String authority) {
-        this(isPublic, authority, null);
-    }
-
-    public CaptureStrategy(boolean isPublic, String authority, String directory) {
-        this.isPublic = isPublic;
-        this.authority = authority;
-        this.directory = directory;
-    }
-}
+ data class CaptureStrategy ( @JvmField val isPublic: Boolean, @JvmField  val authority: String, @JvmField val directory: String? = null)
